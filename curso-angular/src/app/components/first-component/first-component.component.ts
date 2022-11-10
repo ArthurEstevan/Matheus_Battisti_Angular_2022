e9746f4dd4942199a498226444ff556010dfbc2d
ArthurEstevan <arthur.estevan01@gmail.com>
2022-11-10 18:36:03 -0300

    Using Data Interpolation in Angular

--- curso-angular/src/app/components/first-component/first-component.component.ts
+++ curso-angular/src/app/components/first-component/first-component.component.ts
@@ -7,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FirstComponentComponent implements OnInit {
 
+  name: string = "Arthur Estevan Vargas";
+  age: number = 19;
+  job = "Developer";
+  hobbies = ["Correr", "Jogar", "Estudar"]
+  car = {
+    model: "Bugatti",
+    year: 2022,
+    color: "Black"
+  }
+
   constructor() { }
 
   ngOnInit(): void {
